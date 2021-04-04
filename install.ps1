cc39190621b4d17260e57906d4a7eb51e026bb72
redsolver <info@redsolver.net>
2021-04-04 12:58:55 +0200

    Added install.ps1 script for Windows

---
+++ install.ps1
@@ -0,0 +1,42 @@
+#!/usr/bin/env pwsh
+# Based on Deno installer: Copyright 2018 the Deno authors. All rights reserved. MIT license.
+# TODO(everyone): Keep this script simple and easily auditable.
+
+$ErrorActionPreference = 'Stop'
+
+if ($v) {
+  $Version = "v${v}"
+}
+if ($args.Length -eq 1) {
+  $Version = $args.Get(0)
+}
+
+$BinDir = "$Home\AppData\local\skydroid"
+
+$CliExe = "$BinDir\skydroid.exe"
+$Target = 'windows'
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$CliUri = if (!$Version) {
+  "https://github.com/redsolver/skydroid-cli/releases/latest/download/skydroid-${Target}"
+} else {
+  "https://github.com/redsolver/skydroid-cli/releases/download/${Version}/skydroid-${Target}"
+}
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $CliUri -OutFile $CliExe -UseBasicParsing
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "The SkyDroid CLI was installed successfully to $CliExe"
+Write-Output "Run 'skydroid --help' to get started"
